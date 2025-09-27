d45e7a0e87092e9405b3c847f28414540fb4d8fd
Saliha Abid <155230735+salihaabid@users.noreply.github.com>
2025-09-27 13:49:50 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,13 @@ import messageRoutes from './routes/message.route.js';
 import connectDB from './config/mongodb.config.js';
 import cors from 'cors';
 import { app, server } from './config/socket.js';
-import path from 'path';
+// import path from 'path';
 
 dotenv.config();
 // const app = express();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.use(cookieParser());
 app.use(
   cors({
@@ -30,13 +30,13 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
