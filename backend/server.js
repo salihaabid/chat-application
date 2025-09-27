334adedb6591ded4b06cb2d5c10b1a47479a922a
Saliha Abid <155230735+salihaabid@users.noreply.github.com>
2025-09-27 16:29:27 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true,
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,15 +1,12 @@
-import axios from 'axios';
-export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true,
-});
-
 // import axios from 'axios';
-
 // export const axiosInstance = axios.create({
-//   baseURL:
-//     import.meta.env.MODE === 'development'
-//       ? 'http://localhost:5000/api'
-//       : '/api',
+//   baseURL: 'http://localhost:5000/api',
 //   withCredentials: true,
 // });
+
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL, // 👈 use env var
+  withCredentials: true,
+});
