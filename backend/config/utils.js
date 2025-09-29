28eabc18d046764c4bbf20da306f1495dfe3e4df
Saliha Abid <155230735+salihaabid@users.noreply.github.com>
2025-09-29 23:08:16 +0500

    .

--- backend/config/utils.js
+++ backend/config/utils.js
@@ -1,16 +1,35 @@
+// import jwt from 'jsonwebtoken';
+
+// export const generateToken = (userId, res) => {
+//   const token = jwt.sign(
+//     { userId }, // payload
+//     process.env.JWT_SECRET, // secret key
+//     { expiresIn: '1d' } // options
+//   );
+//   res.cookie('jwt', token, {
+//     maxAge: 1 * 24 * 60 * 60 * 1000,
+//     httpOnly: true,
+//     sameSite: 'strict',
+//     secure: process.env.NODE_ENV !== 'DEVELOPMENT',
+//   });
+//   return token;
+// };
+
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign(
-    { userId }, // payload
-    process.env.JWT_SECRET, // secret key
-    { expiresIn: '1d' } // options
-  );
-  res.cookie('jwt', token, {
-    maxAge: 1 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: 'strict',
-    secure: process.env.NODE_ENV !== 'DEVELOPMENT',
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
   });
+
+  res.cookie('jwt', token, {
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    httpOnly: true,
+    // sameSite: process.env.NODE_ENV === 'DEVELOPMENT' ? 'lax' : 'none',
+    // secure: process.env.NODE_ENV !== 'DEVELOPMENT',
+    sameSite: 'none',
+    secure: true,
+  });
+
   return token;
 };
